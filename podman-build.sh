da48d91060e3f2160fb10f98aaadf8ebc2e1daad
HubbeKing <github@hubbe.club>
2021-11-10 18:19:51 +0200

    Remove broken podman-build script

--- podman-build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo podman run --rm --privileged=true --env STORAGE_DRIVER=vfs --volume /home/hubbe/Projects/deepdream-docker/:/src:O quay.io/buildah/stable:latest buildah bud --tag test src
